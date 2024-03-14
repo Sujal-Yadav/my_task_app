283788625508b0e3cdfa12d2655741b5155adee5
Sujal-Yadav <sujjalyadav@gmail.com>
2024-03-15 04:03:39 +0530

    UI Fixes

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { ScrollView, Text, SafeAreaView, View, TextInput, Button, TouchableOpacity } from 'react-native';
-import Task from './components/Task';
+import { ScrollView, Text, SafeAreaView, View, TextInput, Button, TouchableOpacity, Alert } from 'react-native';
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
 
 export default function App() {
   const [taskList, setTaskList] = useState([]);
@@ -16,6 +16,21 @@ export default function App() {
   function handleText(text) {
     setText(text);
   }
+
+  function handleRemoveTask(indexToRemove) {
+    Alert.alert("Want to delete!", 'Delete', [
+      {
+        text: 'Cancel',
+        style: 'cancel'
+      },
+      {
+        text: 'Ok',
+        style: 'destructive'
+      }
+    ])
+    setTaskList(taskList.filter((_, index) => index !== indexToRemove));
+  }
+
   return (
     <ScrollView className="mt-10">
       <View className="flex justify-center items-center mt-10">
@@ -26,17 +41,20 @@ export default function App() {
         <TextInput className="border-2 rounded-sm border-red-500 p-2 w-4/5" onChangeText={handleText} placeholder='Enter your text'></TextInput>
       </View>
 
-      <View className="flex-row justify-center items-center mt-5">
-        <TouchableOpacity className="bg-blue-500 mr-10 p-2 rounded-md" title='ADD NEW TASK' onPress={handleButton}><Text className="text-white">+ ADD NEW TASK</Text></TouchableOpacity>
-        <TouchableOpacity className="bg-red-500 p-2 rounded-md" title='DELETE TASK'><Text className="text-white">- DELETE TASK</Text></TouchableOpacity>
+      <View className="flex justify-center items-center mt-5">
+        <TouchableOpacity className="bg-blue-500 p-2 rounded-md" title='ADD NEW TASK' onPress={handleButton}><Text className="text-white">+ ADD NEW TASK</Text></TouchableOpacity>
       </View>
 
       <View className="flex justify-center items-center mt-10">
         <Text className="text-center text-4xl border-b-2 border-red-500">My Task List</Text>
       </View>
 
-      <View className="flex justify-start mt-4 px-4">
-        {taskList.map((task) => <Text className="text-white text-xl mb-2 p-2 rounded-sm bg-slate-500" >{task}</Text>)}
+      <View className="mt-4 px-4">
+        {taskList.map((task, index) => <View className="flex-row justify-evenly items-center">
+          <Text className="text-white text-xl w-11/12 ml-2 mb-2 p-2 rounded-md bg-slate-500" >{task}</Text>
+          <TouchableOpacity onPress={() => handleRemoveTask(index)} className="flex justify-center mb-2 p-2 ml-2 rounded-md h-11" title='DELETE TASK'><FontAwesome6 name="trash" size={20} duotone={false}/></TouchableOpacity>
+        </View>)}
+
       </View>
 
       <StatusBar style="auto" />
